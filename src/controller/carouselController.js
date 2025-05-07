b97cd3102432440a503003061538c2bf77291425
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-05-07 17:04:15 +0100

    carouselController.js: Wrote hideImages method and called in constuctor. Images now disappear on opening of page

--- src/controller/carouselController.js
+++ src/controller/carouselController.js
@@ -1,11 +1,18 @@
 class CarouselController {
     constructor() {
-        this.carouselImages = document.querySelectorAll(".carouselImage")
+        this.carouselImages = document.querySelectorAll(".carouselImages")
+        this.hideImages();
     }
 
     hideImage(image) {
         image.style.display = "none";
     };
+
+    hideImages() {
+        for (const image of this.carouselImages) {
+            this.hideImage(image);
+        }
+    }
 }
 
 export default CarouselController;
