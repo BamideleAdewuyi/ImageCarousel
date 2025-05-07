98ba6e03874b12069a1d3d2dab678c849cb2a61b
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-05-07 15:22:48 +0100

    carouselController.js: Set up hideImage method

--- src/controller/carouselController.js
+++ src/controller/carouselController.js
@@ -3,6 +3,10 @@ class CarouselController {
         this.carouselImage = document.querySelectorAll(".carouselImage")
         console.log(this.carouselImage)
     }
+
+    hideImage(image) {
+        image.style.display = "none";
+    }
 }
 
 export default CarouselController;
